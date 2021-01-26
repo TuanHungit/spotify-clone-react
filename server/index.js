d30cf421075537ef19432ddd95715f20c1748366
NguyenTuanHung14 <17110156@student.hcmute.edu.vn>
2021-01-26 23:00:36 +0700

    using react-boilerplate to setup project

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const compression = require("compression");
+const bodyParser = require("body-parser");
+const http = require("http");
+const path = require("path");
+const morgan = require("morgan");
+const debug = require("debug");
+
+const info = debug("server:app:info");
+const error = debug("server:app:error");
+
+const routes = require("./app");
+
+const app = express();
+const server = http.createServer(app);
+app.disable("x-powered-by");
+
+// middlewares
+const env = app.get("env");
+if (env === "production") {
+  app.use(
+    morgan("common", {
+      // skip: (req, res) => res.statusCode < 400,
+      stream: path.resolve(__dirname, "/../morgan.log"),
+    })
+  );
+} else {
+  app.use(morgan("dev"));
+}
+
+app.use(compression());
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+// routes
+routes(app);
+
+process.on("uncaughtException", (err) => {
+  error("crashed!!! - " + (err.stack || err));
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { hot } from 'react-hot-loader';
-import React from 'react';
-import AppRouter from './routes';
-
-const App = () => {
-  return <h1>Heelo</h1>;
-};
-
-export default hot(module)(App);
